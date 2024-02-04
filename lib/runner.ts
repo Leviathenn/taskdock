ef34584edce78ca33928bc70fd59c44ea9476e5b
Leviathenn <levixanderson@hottmail.com>
2024-02-03 23:07:50 -0500

    added some syntax :D

---
+++ lib/runner.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Leviathenn
+ */
+
+import * as fs from "fs"
+import Dockerode from "dockerode"
+
+export class runner{
+    private container: Dockerode;
+    constructor(){
+        this.container = new Dockerode();
+    }
+    prep(){
+        this.container.createContainer({Image:""});
+    }
+}
